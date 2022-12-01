f5afaa532f278387ca53ef4e232a47f6cfa4cef0
haripriyatud <haripriyasubramanian@gmail.com>
2022-12-01 13:02:30 +0100

    file file

---
+++ 1.intro.js
@@ -0,0 +1,12 @@
+let amount =9;
+if(amount<10){ 
+    console.log('number smaller than 10');
+}
+else if(amount>10){ 
+    console.log('number larger than 10');
+}
+else{
+console.log('number equal to 10');
+}
+
+console.log('this is my first node app');
