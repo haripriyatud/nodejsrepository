b2c2ca062587c8413f60c066b3d0a204054e5c38
haripriyatud <haripriyasubramanian@gmail.com>
2022-12-01 13:09:15 +0100

    modified first program with cmd line arguments

--- 1.intro.js
+++ 1.intro.js
@@ -1,12 +1,12 @@
-let amount =9;
+let amount = process.argv.slice(2);
 if(amount<10){ 
-    console.log('number smaller than 10');
+    console.log('argument passed is '  + amount + ' which is smaller than 10');
 }
 else if(amount>10){ 
-    console.log('number larger than 10');
+    console.log('argument passed larger than 10');
 }
 else{
-console.log('number equal to 10');
+console.log('argument passed is equal to 10');
 }
 
 console.log('this is my first node app');
